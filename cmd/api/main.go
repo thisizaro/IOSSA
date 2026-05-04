f77a99465f55325bfa83f7dd5a835561778818af
thisizaro <thisisaro.official@gmail.com>
2026-05-04 23:23:50 +0530

    added a / endpoint.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"iossa/internal/handler"
 	"github.com/go-chi/chi/v5"
@@ -14,8 +15,22 @@ import (
 func main() {
 	r := chi.NewRouter()
 
+	// Root route
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("IOSSA API is running 🚀"))
+	})
+
+	// Your main endpoint
 	r.Post("/analyze", handler.AnalyzeHandler)
 
+	// PORT handling (important for Cloud Run)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+
+	
 	log.Println("Server running on :8080")
 	http.ListenAndServe(":8080", r)
 }
